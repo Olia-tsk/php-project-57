d37d9051f8de4aacc37a3cd788fb73fa61eb6d9c
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-11 17:13:04 +0700

    add localization

---
+++ lang/ru/auth.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'name'              => 'Имя',
+    'email'             => 'Email',
+    'password'          => 'Пароль',
+    'confirmation'      => 'Подтверждение',
+    'alreadyRegistered' => 'Уже зарегистрированы?',
+    'register'          => 'Зарегистрировать',
+    'remember'          => 'Запомнить меня',
+    'forgot'            => 'Забыли пароль?',
+    'logIn'             => 'Войти',
+    'reset'             => 'Сбросить пароль',
+];
