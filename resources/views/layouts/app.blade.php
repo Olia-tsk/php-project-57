082936e398275f4bed318bbcd044325384bf6aaf
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-16 18:33:07 +0700

    edit app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,20 +17,12 @@
 </head>
 
 <body>
-    {{-- <div class="min-h-screen bg-gray-100"> --}}
-    {{-- @include('layouts.navigation') --}}
+    <div id="app">
+        <x-header></x-header>
 
-    @isset($header)
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
-            </div>
-        </header>
-    @endisset
+        {{ $slot }}
+    </div>
 
-    {{ $slot }}
-
-    {{-- </div> --}}
 </body>
 
 </html>
