6b7a4c486b1d3779996d58ffda1ecc393fb32534
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-16 18:33:59 +0700

    add statuses page template

---
+++ resources/views/pages/statuses.blade.php
@@ -0,0 +1,48 @@
+<x-app-layout>
+    <section class="bg-white dark:bg-gray-900">
+        <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+            <div class="grid col-span-full">
+                <h1 class="mb-5">Статусы</h1>
+
+                @auth
+                    <div>
+                        <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            @lang('app.pages.createStatus')
+                        </a>
+                    </div>
+                @endauth
+
+                <table class="mt-4">
+                    <thead class="border-b-2 border-solid border-black text-left">
+                        <tr>
+                            <th>ID</th>
+                            <th>@lang('app.pages.name')</th>
+                            <th>@lang('app.pages.createdDate')</th>
+                            @auth
+                                <th>@lang('app.pages.actions')</th>
+                            @endauth
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="border-b border-dashed text-left">
+                            <td>1</td>
+                            <td>новая</td>
+                            <td>16.06.2025</td>
+                            @auth
+                                <td>
+                                    <a data-confirm="@lang('app.pages.confirm')" data-method="delete"
+                                        class="text-red-600 hover:text-red-900" href="#">
+                                        @lang('app.pages.delete')
+                                    </a>
+                                    <a class="text-blue-600 hover:text-blue-900" href="#">
+                                        @lang('app.pages.edit')
+                                    </a>
+                                </td>
+                            @endauth
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+</x-app-layout>
