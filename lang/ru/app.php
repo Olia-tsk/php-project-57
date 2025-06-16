123b0dd1aef21c006542a5fb0f92709752ba7d47
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-16 18:36:01 +0700

    add translations

--- lang/ru/app.php
+++ lang/ru/app.php
@@ -11,6 +11,25 @@ return [
     'description'   => 'Это простой менеджер задач на Laravel',
     'mainButton'    => 'Нажми меня',
     'logout'        => 'Выход',
-    ''              => '',
-    ''              => '',
+
+    'pages' => [
+        'tasks'         => 'Задачи',
+        'statuses'      => 'Статусы',
+        'status'        => 'Статус',
+        'labels'        => 'Метки',
+        'author'        => 'Автор',
+        'executor'      => 'Исполнитель',
+        'createdDate'   => 'Дата создания',
+        'name'          => 'Имя',
+        'apply'         => 'Применить',
+        'description'   => 'Описание',
+        'actions'       => 'Действия',
+        'delete'        => 'Удалить',
+        'edit'          => 'Изменить',
+        'createLabel'   => 'Создать метку',
+        'createStatus'  => 'Создать статус',
+        'createTask'    => 'Создать задачу',
+        'confirm'       => 'Вы уверены?',
+    ],
+
 ];
