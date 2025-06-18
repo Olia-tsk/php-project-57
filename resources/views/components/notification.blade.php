cf2db6fc37b7aa27a60297b825fdc2e09a87fa46
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-18 16:36:25 +0700

    create notification component

---
+++ resources/views/components/notification.blade.php
@@ -0,0 +1,19 @@
+@if (Session::has('flash_message'))
+    <div class="p-4 bg-green-50 rounded-md alert alert-success" role="alert">
+        <div class="flex">
+            <div class="shrink-0">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
+                    data-slot="icon" class="size-5 text-[#06df72]">
+                    <path fill-rule="evenodd"
+                        d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z"
+                        clip-rule="evenodd"></path>
+                </svg>
+            </div>
+            <div class="ml-3">
+                <h3 class="text-sm font-medium text-[#026630]">
+                    {{ Session::get('flash_message') }}
+                </h3>
+            </div>
+        </div>
+    </div>
+@endif
