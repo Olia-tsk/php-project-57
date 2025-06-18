386d6a7fd2027c525f57946375b1ab55993a8bb9
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-18 17:57:28 +0700

    add factory

--- app/Models/TaskStatus.php
+++ app/Models/TaskStatus.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TaskStatus extends Model
 {
+    use HasFactory;
     protected $fillable = ['name'];
 
     public function getCreatedAtAttribute($date): string
