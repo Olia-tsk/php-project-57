881061d187d32fc4e69e8eca63780bf6de215512
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-19 19:08:11 +0700

    update route

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -33,7 +33,7 @@
             <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
                 <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     <li>
-                        <a href="{{ route('tasks') }}"
+                        <a href="{{ route('tasks.index') }}"
                             class="nav-menu-item @ifCurrent('tasks') text-blue-700 @else text-gray-700 @endifCurrent">
                             @lang('app.tasks')
                         </a>
