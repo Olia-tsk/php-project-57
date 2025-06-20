c9d92e99cc86637368e8472e887d88a3e1cae5a8
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-20 19:43:52 +0700

    refactor TaskFactory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,23 +2,30 @@
 
 namespace Database\Factories;
 
+use App\Models\Task;
 use App\Models\TaskStatus;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
- */
 class TaskFactory extends Factory
 {
+    protected $model = Task::class;
+
     public function definition(): array
     {
+        $status = TaskStatus::inRandomOrder()->first() ?? TaskStatus::factory()->create();
+
+        $creator = User::factory()->create();
+        $executor = $this->faker->boolean(50) ? User::factory()->create() : null;
+
         return [
             'name' => $this->faker->sentence(2),
             'description' => $this->faker->sentences(3, true),
-            'status_id' => $this->faker->numberBetween(1, 4),
-            'created_by_id' => User::factory(),
-            'assigned_to_id' => User::factory(),
+            'status_id' => $status->id,
+            'created_by_id' => $creator->id,
+            'assigned_to_id' => $executor ? $executor->id : null,
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
