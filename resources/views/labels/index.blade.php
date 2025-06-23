9ea47d962274cd5d81ead1ac6042e040ec5d79cc
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-23 17:46:58 +0700

    add labels index template

---
+++ resources/views/labels/index.blade.php
@@ -0,0 +1,56 @@
+<x-app-layout>
+    <section class="bg-white dark:bg-gray-900">
+        <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+            <div class="grid col-span-full">
+
+                <x-notification></x-notification>
+
+                <h1 class="mb-5">@lang('app.pages.labels')</h1>
+
+                @auth
+                    <div>
+                        <a href="{{ route('labels.create') }}" class="blue-button">
+                            @lang('app.pages.createLabel')
+                        </a>
+                    </div>
+                @endauth
+
+                <table class="mt-4">
+                    <thead class="border-b-2 border-solid border-black text-left">
+                        <tr>
+                            <th>ID</th>
+                            <th>@lang('app.pages.name')</th>
+                            <th>@lang('app.pages.description')</th>
+                            <th>@lang('app.pages.createdDate')</th>
+                            @auth
+                                <th>@lang('app.pages.actions')</th>
+                            @endauth
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($labels as $label)
+                            <tr class="border-b border-dashed text-left">
+                                <td>{{ $label->id }}</td>
+                                <td>{{ $label->name }}</td>
+                                <td>{{ $label->description }}</td>
+                                <td>{{ $label->created_at }}</td>
+                                @auth
+                                    <td>
+                                        <a data-confirm="@lang('app.pages.confirm')" data-method="delete" class="delete-link"
+                                            href="{{ route('labels.destroy', $label) }}">
+                                            @lang('app.pages.delete')
+                                        </a>
+                                        <a class="edit-link " href="{{ route('labels.edit', $label) }}">
+                                            @lang('app.pages.edit')
+                                        </a>
+                                    </td>
+                                @endauth
+                            </tr>
+                        @endforeach
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+</x-app-layout>
