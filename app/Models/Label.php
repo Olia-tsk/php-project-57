74aa8d3d8794226ac7e44ca4407ad23c1b07cd76
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-23 17:43:12 +0700

    create Label model

---
+++ app/Models/Label.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Label extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description'];
+
+    public function tasks()
+    {
+        return $this->belongsToMany(Task::class);
+    }
+
+    public function getCreatedAtAttribute($date): string
+    {
+        return $date ? Carbon::parse($date)->format('d.m.Y') : '';
+    }
+}
