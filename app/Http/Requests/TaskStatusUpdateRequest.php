d0917baacecc2f2852c4535819b9d5a22a5ce610
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-24 18:06:52 +0700

    create TaskStatusUpdateRequest

---
+++ app/Http/Requests/TaskStatusUpdateRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\TaskStatus;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TaskStatusUpdateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        $taskStatus = $this->route('task_status') ?? new TaskStatus();
+        return $this->user() && $this->user()->can('update', $taskStatus);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                Rule::unique('task_statuses')->ignore($this->route('task_status')->id),
+            ],
+        ];
+    }
+}
