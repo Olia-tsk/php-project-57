5272f6caf256793e2b55a5a72cdb6060f4fd969c
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-24 18:24:29 +0700

    create LabelUpdateRequest

---
+++ app/Http/Requests/LabelUpdateRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Label;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class LabelUpdateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        $label = $this->route('task_status') ?? new Label();
+        return $this->user() && $this->user()->can('update', $label);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                Rule::unique('labels')->ignore($this->route('label')->id),
+            ],
+            'description' => 'nullable',
+        ];
+    }
+}
