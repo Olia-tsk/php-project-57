9ed1cfeae8780d5b63a42301dd003424c97c7745
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-24 18:24:14 +0700

    create LabelStoreRequest

---
+++ app/Http/Requests/LabelStoreRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Label;
+use Illuminate\Foundation\Http\FormRequest;
+
+class LabelStoreRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        $label = $this->route('label') ?? new Label();
+        return $this->user() && $this->user()->can('create', $label);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|unique:labels',
+            'description' => 'nullable',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.unique' => 'Метка с таким именем уже существует',
+        ];
+    }
+}
