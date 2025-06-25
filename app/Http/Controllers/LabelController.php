5c6bb86313b014506c641f9548fb3aaa16097c76
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-25 11:45:49 +0700

    refactor validation

--- app/Http/Controllers/LabelController.php
+++ app/Http/Controllers/LabelController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LabelStoreRequest;
+use App\Http\Requests\LabelUpdateRequest;
 use App\Models\Label;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Validation\Rule;
 
 class LabelController extends Controller
 {
+    use AuthorizesRequests;
+
     public function index()
     {
         $labels = Label::orderBy('id', 'asc')->get();
@@ -19,20 +21,15 @@ class LabelController extends Controller
 
     public function create()
     {
-        if (!Auth::check()) {
-            abort(403, 'Unauthorized action');
-        }
+        $this->authorize('create', Label::class);
 
         $label = new Label();
         return view('labels.create', compact('label'));
     }
 
-    public function store(Request $request)
+    public function store(LabelStoreRequest $request)
     {
-        $data = $request->validate([
-            'name' => 'required|unique:labels',
-            'description' => 'nullable',
-        ]);
+        $data = $request->validated();
 
         $label = new Label();
         $label->fill($data);
@@ -45,27 +42,19 @@ class LabelController extends Controller
 
     public function show(Label $label)
     {
-        //
+        $this->authorize('view', $label);
     }
 
     public function edit(Label $label)
     {
-        if (!Auth::check()) {
-            abort(403, 'Unauthorized action');
-        }
+        $this->authorize('update', $label);
 
         return view('labels.edit', compact('label'));
     }
 
-    public function update(Request $request, Label $label)
+    public function update(LabelUpdateRequest $request, Label $label)
     {
-        $data = $request->validate([
-            'name' => [
-                'required',
-                Rule::unique('labels')->ignore($label),
-            ],
-            'description' => 'nullable',
-        ]);
+        $data = $request->validated();
 
         $label->update($data);
 
@@ -76,6 +65,8 @@ class LabelController extends Controller
 
     public function destroy(Label $label)
     {
+        $this->authorize('delete', $label);
+
         if ($label->tasks()->exists()) {
             Session::flash('flash_message_error', __('app.flash.label.deleteFailed'));
             return redirect()->route('labels.index');
