925bcd9160091ff5fc663fd049330f4d2b7c6188
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-26 15:32:01 +0700

    cleanup routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\LabelController;
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskStatusController;
 use Illuminate\Support\Facades\Route;
@@ -15,15 +14,5 @@ Route::resource('tasks', TaskController::class);
 Route::resource('task_statuses', TaskStatusController::class);
 
 Route::resource('labels', LabelController::class);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__ . '/auth.php';
