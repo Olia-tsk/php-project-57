065e18f9e450e1dbb137b8ef3f388b54f7b4c224
Ольга Курбатова <kon@2i.tusur.ru>
2025-06-26 16:01:34 +0700

    update translations

--- lang/ru/validation.php
+++ lang/ru/validation.php
@@ -3,4 +3,13 @@
 return [
     'required'  => 'Это обязательное поле',
     'unique'    => 'Поле с таким именем уже существует',
+    'confirmed' => ':attribute не совпадает.',
+    'email' => ':attribute введён некорректно.',
+
+    'min' => [
+        'string' => ':attribute должен быть как минимум :min символов.',
+    ],
+    'attributes' => [
+        'password' => 'Пароль',
+    ],
 ];
