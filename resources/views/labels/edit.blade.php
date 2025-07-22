6f3ae351d624d404580685d1ed26b3412c150081
Ольга Курбатова <kon@2i.tusur.ru>
2025-07-22 16:15:35 +0700

    refactor html for better mobile version

--- resources/views/labels/edit.blade.php
+++ resources/views/labels/edit.blade.php
@@ -1,40 +1,30 @@
 <x-app-layout>
     <section class="bg-white">
-        <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
-            <div class="grid col-span-full">
-                <h1 class="mb-5">@lang('app.pages.updateLabel')</h1>
+        <div class="max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:py-16 lg:pt-28">
 
-                {{ html()->modelForm($label, 'PATCH', route('labels.update', $label))->class('w-50')->open() }}
+            <h1 class="mb-5 page-title">@lang('app.pages.updateLabel')</h1>
 
-                {{ html()->div()->class('flex flex-col')->open() }}
+            {{ html()->modelForm($label, 'PATCH', route('labels.update', $label))->open() }}
 
-                <div>
-                    {{ html()->label(__('app.pages.name'), 'name') }}
-                </div>
+            {{ html()->div()->class('flex flex-col')->open() }}
 
-                <div class="mt-2">
-                    {{ html()->text('name')->class('form-field')->classIf($errors->has('name'), 'border-rose-600') }}
-                    @if ($errors->has('name'))
-                        <p class="text-rose-600">{{ $errors->first('name') }}</p>
-                    @endif
-                </div>
+            {{ html()->label(__('app.pages.name'), 'name') }}
 
-                <div class="mt-2">
-                    {{ html()->label(__('app.pages.description'), 'description') }}
-                </div>
+            {{ html()->text('name')->class('form-field')->classIf($errors->has('name'), 'border-rose-600') }}
+            @if ($errors->has('name'))
+                <p class="text-rose-600">{{ $errors->first('name') }}</p>
+            @endif
 
-                <div class="mt-2">
-                    {{ html()->textarea('description')->class('form-field h-32') }}
+            {{ html()->label(__('app.pages.description'), 'description')->class('mt-2') }}
 
-                </div>
+            {{ html()->textarea('description')->class('form-field h-32') }}
 
-                <div class="mt-2">
-                    {{ html()->submit(__('app.pages.update'))->class('blue-button') }}
-                </div>
-                {{ html()->div()->close() }}
+            {{ html()->submit(__('app.pages.update'))->class('blue-button mt-2') }}
 
-                {{ html()->closeModelForm() }}
-            </div>
+            {{ html()->div()->close() }}
+
+            {{ html()->closeModelForm() }}
+
         </div>
     </section>
 </x-app-layout>
